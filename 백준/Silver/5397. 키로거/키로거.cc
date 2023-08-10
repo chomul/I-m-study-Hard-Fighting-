db4f6ac3c91189c793e0d13ae3af9bb148bd8c0d
Sung Chan <102793335+chomul@users.noreply.github.com>
2023-08-10 16:08:09 +0900

    [Silver II] Title: 키로거, Time: 112 ms, Memory: 3696 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string s;
+	int m;
+	cin >> m;
+
+	while (m)
+	{
+		stack<char> left, right;
+		cin >> s;
+
+		for (int i = 0; i < s.length(); i++)
+		{
+			if (!left.empty() && s[i] == '<')
+			{
+				right.push(left.top());
+				left.pop();
+			}
+			else if (!right.empty() && s[i] == '>')
+			{
+				left.push(right.top());
+				right.pop();
+			}
+			else if (!left.empty() && s[i] == '-')
+				left.pop();
+			else if(s[i] != '<' && s[i] != '>'&& s[i] != '-')
+			{
+				left.push(s[i]);
+			}
+		}
+
+		while (!left.empty())
+		{
+			right.push(left.top());
+			left.pop();
+		}
+		while (!right.empty())
+		{
+			cout << right.top();
+			right.pop();
+		}
+		cout << '\n';
+		m--;
+	}
+	
+	return 0;
+}
