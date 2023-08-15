3acb2948db0bfe1e638b68a92612199bab68df8b
Sung Chan <102793335+chomul@users.noreply.github.com>
2023-08-16 00:52:32 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2544 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,11 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int board[1002][1002] = { 0 };
 int vis[100002];
-int n = 7, m = 10, cnt = 0;
-int dx[4] = { 1,0,-1,0 };
-int dy[4] = { 0,1,0,-1 };
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -13,15 +9,28 @@ int main() {
 	cout.tie(NULL);
 
 	queue<int> Q;
+	int n, m;
 	cin >> n >> m;
 	fill(vis, vis + 100001, -1);
 	Q.push(n);
 	vis[n] = 0;
-	while (vis[m] == -1)
+
+	if (n == m)
+	{
+		cout << 0;
+		return 0;
+	}
+
+	while (!Q.empty())
 	{
 		auto cur = Q.front();
 		Q.pop();
 
+		if (vis[m] > 0)
+		{
+			cout << vis[m];
+			return 0;
+		}
 
 		for (int nxt : {2 * cur, cur + 1, cur - 1})
 		{
@@ -30,10 +39,7 @@ int main() {
 
 			Q.push(nxt);
 			vis[nxt] = vis[cur] + 1;
-
 		}
 	}
-
-	cout << vis[m];
 	return 0;
 }
