25101e8401eb5b68771bc6f8cef7010b8d47e237
Sung Chan <102793335+chomul@users.noreply.github.com>
2023-09-03 14:45:30 +0900

    [Silver III] Title: N과 M (3), Time: 380 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[10];
+int tot[10];
+int n, m;
+
+void fun(int cnt)
+{
+	if (cnt == m)
+	{
+		for (int i = 0; i < m; i++)
+		{
+			if (tot[i] == 0) break;
+			cout << tot[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	
+	for (int i = 1; i <= n; i++)
+	{
+		tot[cnt] = i;
+		fun(cnt + 1);
+		tot[cnt] = 0;
+
+	}
+
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++)
+	{
+		arr[i - 1] = i;
+	}
+
+	fun(0);
+
+	return 0;
+}
