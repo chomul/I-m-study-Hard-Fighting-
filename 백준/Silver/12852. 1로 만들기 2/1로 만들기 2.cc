9f9f7d5b96c5eddb0b946b46c537522a4a1db8ab
Sung Chan <102793335+chomul@users.noreply.github.com>
2023-09-10 15:52:15 +0900

    [Silver I] Title: 1로 만들기 2, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int d[1000005];
+int ch[1000005];
+int n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+
+	d[1] = 0;
+
+	for (int i = 2; i <= n; i++)
+	{
+		d[i] = d[i - 1] + 1;
+		ch[i] = i - 1;
+		if (i % 3 == 0)
+		{
+			d[i] = min(d[i], d[i / 3] + 1);
+			if (d[i] >= d[i / 3] + 1)
+				ch[i] = i / 3;
+		}
+
+
+		if (i % 2 == 0)
+		{
+			d[i] = min(d[i], d[i / 2] + 1);
+			if (d[i] >= d[i / 2] + 1)
+				ch[i] = i / 2;
+		}
+
+	}
+
+	cout << d[n] << '\n';
+
+	while (n)
+	{
+		cout << n << ' ';
+		n = ch[n];
+	}
+
+	return 0;
+}
