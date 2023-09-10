dcaef937256d1c59a759d66ce77004b3b7877c02
Sung Chan <102793335+chomul@users.noreply.github.com>
2023-09-10 16:50:23 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <array>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <stack>
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	vector<int> vec(n + 1);
+	vec[1] = 1;
+	vec[2] = 2;
+
+	for (int i = 3; i <= n; i++)
+	{
+		vec[i] = vec[i - 1] + vec[i - 2];
+		vec[i] %= 10007;
+	}
+
+	cout << vec[n];
+
+
+	return 0;
+}
+
