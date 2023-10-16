4f816ea744e0c642ff579e843d6d101d887acbfb
Sung Chan <102793335+chomul@users.noreply.github.com>
2023-10-16 23:56:33 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 48 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -1,21 +1,30 @@
-#include<iostream> 
-#include<algorithm> 
-#include<vector> 
-#include<cmath>
-#include<string>
+#include <bits/stdc++.h>
+#include "unordered_set"
 using namespace std;
 
-int main() {
-	int n;
-	cin >> n;
-	vector<pair<int, int>> arr;
-	for (int i = 0; i < n; i++) {
-		int a, b;
-		cin >> a >> b;
-		arr.push_back({ a, b });
-	}
-	sort(arr.begin(), arr.end());
+bool compare(pair<int, int> a, pair<int, int> b)
+{
+    if (a.first != b.first)
+        return a.first < b.first;
+    else
+        return a.second < b.second;
+}
 
-	for (auto c : arr)
-		cout << c.first << " " << c.second << '\n';
+int main(void) 
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    vector<pair<int, int>> vec(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> vec[i].first >> vec[i].second;
+
+    sort(vec.begin(), vec.end(), compare);
+
+    for (auto c : vec)
+        cout << c.first << ' ' << c.second << '\n';
+
 }
