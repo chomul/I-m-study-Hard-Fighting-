f4d602a7ffbd6c695272138824f4bcd0112f89c3
Sung Chan <102793335+chomul@users.noreply.github.com>
2025-07-07 10:52:07 +0900

    [level 2] Title: 튜플, Time: 8.12 ms, Memory: 5.65 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.cpp
+++ 프로그래머스/2/64065. 튜플/튜플.cpp
@@ -6,40 +6,41 @@
 using namespace std;
 
 vector<int> solution(string s) {
+ vector<int> answer;
 
-    vector<int> answer;
-
-    unordered_map<int, int> cnt_map;
-    vector<pair<int, int>> v;
+    // 숫자 등장 횟수 카운트 (1~100000 범위 가정)
+    vector<int> count(100001, 0);
     string str;
-    for (int i = 0; i < s.length(); i++)
-    {
-        if (s[i] == '{')
-            continue;
-        else if (isdigit(s[i]))
-        {
+
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '{') continue;
+
+        if (isdigit(s[i])) {
             str += s[i];
         }
-        else if (s[i] == ',' || s[i] == '}')
-        {
-            if (!str.empty())
-            {
-                cnt_map[stoi(str)]++;
+        else if (s[i] == ',' || s[i] == '}') {
+            if (!str.empty()) {
+                int num = stoi(str);
+                count[num]++;
                 str.clear();
             }
         }
     }
 
-    for (auto c : cnt_map)
-        v.push_back({c.first, c.second});
+    // 등장 횟수를 기반으로 정렬
+    vector<pair<int, int>> v;
+    for (int i = 1; i < count.size(); ++i) {
+        if (count[i] > 0) {
+            v.push_back({i, count[i]});
+        }
+    }
 
-    sort(v.begin(), v.end(), [](const pair<int, int>& a, const pair<int, int>& b)
-    {
+    sort(v.begin(), v.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
         return a.second > b.second;
     });
 
-    for (auto c : v)
-        answer.push_back(c.first);
-    
+    for (auto& p : v)
+        answer.push_back(p.first);
+
     return answer;
 }
