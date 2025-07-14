cb5a976a49c3ccae07169a7d715ca960f1867911
Sung Chan <102793335+chomul@users.noreply.github.com>
2025-07-14 13:36:30 +0900

    [Gold IV] Title: 플로이드, Time: 24 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,39 +1,49 @@
-#include <bits/stdc++.h>
-#include <unordered_set>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
 #include <unordered_map>
+#include <unordered_set>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
 int floid[105][105];
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+int main(int argc, char* argv[])
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
     int n, m;
     cin >> n >> m;
 
     for (int i = 1; i <= n; i++)
-        fill(floid[i], floid[i] + 1 + n, 0x3f3f3f3f);   // 기본 무한대 설정
-    for (int i = 1; i <= n; i++) floid[i][i] = 0;       // 자기 자신 0 설정
+        fill(floid[i], floid[i] + n + 1, 0x3f3f3f3f);
+    for (int i = 1; i <= n; i++) floid[i][i] = 0;
 
-    for (int i = 0; i < m; i++)
+    for (int i = 1; i <= m; i++)
     {
         int a, b, c;
         cin >> a >> b >> c;
-        floid[a][b] = min(floid[a][b], c);              // min 사용 이유는 문제 조건때문
+        floid[a][b] = min(floid[a][b], c);
     }
 
     for (int k = 1; k <= n; k++)
-    {
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= n; j++)
-            {   // min 과 같은 의미이지만 시간상으로 더 유리
-                if(floid[i][k] + floid[k][j] < floid[i][j]) floid[i][j] = floid[i][k] + floid[k][j]; 
-                // floid[i][j] = min(floid[i][j], floid[i][k] + floid[k][j]);
-            }
-        }
-    }
+        for (int i = 1; i <= n; i++)        
+            for (int j = 1; j <= n; j++)            
+                floid[i][j] = min(floid[i][j], floid[i][k] + floid[k][j]);
+
 
     for (int i = 1; i <= n; i++)
     {
@@ -48,3 +58,4 @@ int main() {
 
     return 0;
 }
+
